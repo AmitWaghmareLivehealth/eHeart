f485b09ebc2b21f8f6f80ace882b095cc5eacb7f
Amit <amit.waghamare@liveheath.in>
2020-03-26 17:15:33 +0530

    check of pdf added

--- app/scenes/container/report.view/ImageScrollView.js
+++ app/scenes/container/report.view/ImageScrollView.js
@@ -1,29 +1,47 @@
 import React, {Component} from 'react';
 import {Text, ScrollView, Image, TouchableOpacity} from 'react-native';
 import Global from '../../../utils/const/globals';
+import {View} from 'native-base';
 
 export default class ImageScrollView extends Component {
   render() {
     const {images, onImageSelect} = this.props;
+
     return (
       <ScrollView horizontal={true} style={{flex: 1}}>
-        {images.map(image => (
-          <TouchableOpacity
-            onPress={() => {
-              onImageSelect(image);
-            }}>
-            <Image
-              source={{uri: image.url}}
-              style={{
-                height: 200,
-                marginTop: 20,
-                width: Global.screenWidth - 120,
-                marginLeft: 10,
-                flex: 1,
-              }}
-            />
-          </TouchableOpacity>
-        ))}
+        {images.map(image => {
+          if (!image || !image.url) {
+            return (
+              <View>
+                <Text>Invalid URL</Text>
+              </View>
+            );
+          }
+          const isPDF = image.url.split('.').pop() === 'pdf';
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                if (!isPDF) {
+                  onImageSelect(image);
+                }
+              }}>
+              <Image
+                source={
+                  isPDF
+                    ? require('../../../assets/common/pdf.png')
+                    : {uri: image.url}
+                }
+                style={{
+                  height: 200,
+                  marginTop: 20,
+                  width: Global.screenWidth - 120,
+                  marginLeft: 10,
+                  flex: 1,
+                }}
+              />
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
     );
   }
