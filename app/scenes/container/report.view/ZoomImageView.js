57997e0c72d8436b02e2ef7697b31e6aa25bb970
Amit <amit.waghamare@liveheath.in>
2020-04-08 10:35:04 +0530

    zoom image effect perfectly solved

--- app/scenes/container/report.view/ZoomImageView.js
+++ app/scenes/container/report.view/ZoomImageView.js
@@ -8,11 +8,13 @@ import {
   Image,
   Text,
   Linking,
+  Dimensions,
 } from 'react-native';
 import Global from '../../../utils/const/globals';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
 import {Card, CardItem, Body, Right} from 'native-base';
+import ImageZoom from 'react-native-image-pan-zoom';
+
 const viewPropTypes = ViewPropTypes || View.propTypes;
 
 export default class PinchZoomView extends Component {
@@ -43,84 +45,11 @@ export default class PinchZoomView extends Component {
     this.distant = 150;
   }
 
-  componentWillMount() {
-    this.gestureHandlers = PanResponder.create({
-      onStartShouldSetPanResponder: false,
-      onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder,
-      onPanResponderGrant: this._handlePanResponderGrant,
-      onPanResponderMove: this._handlePanResponderMove,
-      onPanResponderRelease: this._handlePanResponderEnd,
-      onPanResponderTerminationRequest: (evt) => false,
-      onShouldBlockNativeResponder: (evt) => false,
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-    });
-  }
-
-  _handleStartShouldSetPanResponder = (e, gestureState) => {
-    return false;
-  };
-
-  _handleMoveShouldSetPanResponder = (e, gestureState) => {
-    return true;
-  };
-
-  _handlePanResponderGrant = (e, gestureState) => {
-    if (gestureState.numberActiveTouches === 2) {
-      let dx = Math.abs(
-        e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX,
-      );
-      let dy = Math.abs(
-        e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY,
-      );
-      let distant = Math.sqrt(dx * dx + dy * dy);
-      this.distant = distant;
-    }
-  };
-
-  _handlePanResponderEnd = (e, gestureState) => {
-    this.setState({
-      lastX: this.state.offsetX,
-      lastY: this.state.offsetY,
-      lastScale: this.state.scale,
-    });
-  };
-
-  _handlePanResponderMove = (e, gestureState) => {
-    // zoom
-    if (gestureState.numberActiveTouches === 2) {
-      let dx = Math.abs(
-        e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX,
-      );
-      let dy = Math.abs(
-        e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY,
-      );
-      let distant = Math.sqrt(dx * dx + dy * dy);
-      let scale = (distant / this.distant) * this.state.lastScale;
-      //check scale min to max hello
-      if (scale < this.props.maxScale && scale > this.props.minScale) {
-        this.setState({scale, lastMovePinch: true});
-      }
-    }
-    // translate
-    else if (gestureState.numberActiveTouches === 1) {
-      if (this.state.lastMovePinch) {
-        gestureState.dx = 0;
-        gestureState.dy = 0;
-      }
-      let offsetX = this.state.lastX + gestureState.dx / this.state.scale;
-      let offsetY = this.state.lastY + gestureState.dy / this.state.scale;
-      // if ( offsetX < 0  || offsetY <  0 )
-      this.setState({offsetX, offsetY, lastMovePinch: false});
-    }
-  };
-
-  onDownload=(image)=>{
-    Linking.openURL(image.url).catch(err =>
+  onDownload = (image) => {
+    Linking.openURL(image.url).catch((err) =>
       console.error('An error occurred', err),
     );
-  }
+  };
   render() {
     const {image} = this.props;
     console.log(image);
@@ -147,43 +76,29 @@ export default class PinchZoomView extends Component {
                 name={'file-download'}
                 size={30}
                 color="black"
-                onPress={()=>{this.onDownload(image)}}
+                onPress={() => {
+                  this.onDownload(image);
+                }}
                 underlayColor="transparent"
                 backgroundColor="transparent"
               />
             </Right>
           </View>
         </Card>
-        <View
-          {...this.gestureHandlers.panHandlers}
-          style={[
-            styles.container,
-            this.props.style,
-            {
-              transform: [
-                {scaleX: this.state.scale},
-                {scaleY: this.state.scale},
-                {translateX: this.state.offsetX},
-                {translateY: this.state.offsetY},
-              ],
-            },
-          ]}>
+        <ImageZoom
+          cropWidth={Dimensions.get('window').width}
+          cropHeight={Dimensions.get('window').height}
+          imageWidth={200}
+          imageHeight={200}>
           <Image
-            style={{
-              height: 200,
-              width: Global.screenWidth - 120,
-              marginLeft: '20%',
+            style={{width: 200, height: 200}}
+            source={{
+              uri: image.url,
             }}
-            source={{uri: image.url}}
           />
-        </View>
+        </ImageZoom>
       </>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: '50%',
-  },
-});
+ 
